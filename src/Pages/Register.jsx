a8b6f1e2d817bf3c57e11d244eaaac781bd425e6
Ishraq Sikder <nirzon.sikderbd@gmail.com>
2024-10-23 23:05:59 +0600

    database content show

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -12,110 +12,81 @@ const Register = () => {
     confirmPassword: "",
   });
 
-  const [errors, setErrors] = useState({
-    firstName: "",
-    lastName: "",
-    dob: "",
-    address: "",
-    phoneNumber: "",
-    userEmail: "",
-    userPassword: "",
-    confirmPassword: "",
-  });
+  const [errors, setErrors] = useState({});
+  const [users, setUsers] = useState([]);
 
   const validateForm = () => {
     let isValid = true;
     let errors = {};
 
-    if (!formData.firstName) {
-      errors.firstName = "First name is required";
-      isValid = false;
-    }
-
-    if (!formData.lastName) {
-      errors.lastName = "Last name is required";
-      isValid = false;
-    }
-
-    if (!formData.dob) {
-      errors.dob = "Date of birth is required";
-      isValid = false;
-    }
-
-    if (!formData.address) {
-      errors.address = "Address is required";
-      isValid = false;
-    }
-
-    if (!formData.phoneNumber) {
-      errors.phoneNumber = "Phone number is required";
-      isValid = false;
-    } else if (!/^\d{10,15}$/.test(formData.phoneNumber)) {
+    // Validation logic
+    if (!formData.firstName) errors.firstName = "First name is required";
+    if (!formData.lastName) errors.lastName = "Last name is required";
+    if (!formData.dob) errors.dob = "Date of birth is required";
+    if (!formData.address) errors.address = "Address is required";
+    if (!formData.phoneNumber) errors.phoneNumber = "Phone number is required";
+    else if (!/^\d{10,15}$/.test(formData.phoneNumber))
       errors.phoneNumber = "Invalid phone number";
-      isValid = false;
-    }
 
-    if (!formData.userEmail) {
-      errors.userEmail = "Email is required";
-      isValid = false;
-    } else if (
+    if (!formData.userEmail) errors.userEmail = "Email is required";
+    else if (
       !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
         formData.userEmail
       )
-    ) {
-      errors.userEmail = "Invalid Email";
-      isValid = false;
-    }
+    )
+      errors.userEmail = "Invalid email";
 
-    if (!formData.userPassword) {
-      errors.userPassword = "Password is required";
-      isValid = false;
-    } else if (formData.userPassword.length < 6) {
+    if (!formData.userPassword) errors.userPassword = "Password is required";
+    else if (formData.userPassword.length < 6)
       errors.userPassword = "Password must be at least 6 characters";
-      isValid = false;
-    } else if (!/^(?=.*[a-z])(?=.*[A-Z])/.test(formData.userPassword)) {
+    else if (!/^(?=.*[a-z])(?=.*[A-Z])/.test(formData.userPassword))
       errors.userPassword =
         "At least one lowercase letter and one uppercase letter";
-      isValid = false;
-    }
 
-    if (formData.confirmPassword !== formData.userPassword) {
+    if (formData.confirmPassword !== formData.userPassword)
       errors.confirmPassword = "Passwords do not match";
-      isValid = false;
-    }
 
     setErrors(errors);
-    return isValid;
+    return Object.keys(errors).length === 0;
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (validateForm()) {
       fetch("http://localhost:5000/register", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       })
         .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-        });
-
-      setFormData({
-        firstName: "",
-        lastName: "",
-        dob: "",
-        address: "",
-        phoneNumber: "",
-        userEmail: "",
-        userPassword: "",
-        confirmPassword: "",
-      });
+        .then(() => {
+          // Clear form data
+          setFormData({
+            firstName: "",
+            lastName: "",
+            dob: "",
+            address: "",
+            phoneNumber: "",
+            userEmail: "",
+            userPassword: "",
+            confirmPassword: "",
+          });
+          fetchUsers(); // Fetch updated user list
+        })
+        .catch((err) => console.error("Error:", err));
     }
   };
+
+  const fetchUsers = () => {
+    fetch("http://localhost:5000/get-user")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error("Error fetching users:", err));
+  };
+
+  useEffect(() => {
+    fetchUsers(); // Fetch users when the component mounts
+  }, []);
 
   const handleInputChange = (e) => {
     setFormData({
@@ -125,143 +96,135 @@ const Register = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-[80vh] bg-gradient-to-b from-gray-900 via-gray-800 to-gray-700">
-      <div className="flex bg-gray-800 flex-col lg:mt-10 md:w-[600px] p-10 pb-4 pt-2 rounded-xl shadow-lg border border-gray-600">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
         <div className="mb-4 text-center border-b-2 border-gray-600">
           <h1 className="my-2 text-4xl font-extrabold text-white">Register</h1>
         </div>
-        <form
-          onSubmit={handleSubmit}
-          className="grid grid-cols-1 md:grid-cols-2 gap-4 space-y-0">
+        <form onSubmit={handleSubmit} className="grid gap-4">
           <div>
-            <label className="block mb-2 text-sm text-gray-300">
-              First Name
-            </label>
+            <label htmlFor="firstName">First Name</label>
             <input
               type="text"
+              id="firstName"
               name="firstName"
-              placeholder="Enter your first name"
-              className="w-full px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white"
               value={formData.firstName}
               onChange={handleInputChange}
+              className="w-full outline-none px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white focus:border-blue-500"
             />
             {errors.firstName && (
-              <small className="text-red-500">{errors.firstName}</small>
+              <p className="text-red-500">{errors.firstName}</p>
             )}
           </div>
           <div>
-            <label className="block mb-2 text-sm text-gray-300">
-              Last Name
-            </label>
+            <label htmlFor="lastName">Last Name</label>
             <input
               type="text"
+              id="lastName"
               name="lastName"
-              placeholder="Enter your last name"
-              className="w-full px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white"
               value={formData.lastName}
               onChange={handleInputChange}
+              className="w-full outline-none px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white focus:border-blue-500"
             />
             {errors.lastName && (
-              <small className="text-red-500">{errors.lastName}</small>
+              <p className="text-red-500">{errors.lastName}</p>
             )}
           </div>
           <div>
-            <label className="block mb-2 text-sm text-gray-300">
-              Date of Birth
-            </label>
+            <label htmlFor="dob">Date of Birth</label>
             <input
               type="date"
+              id="dob"
               name="dob"
-              className="w-full px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white"
               value={formData.dob}
               onChange={handleInputChange}
+              className="w-full outline-none px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white focus:border-blue-500"
             />
-            {errors.dob && <small className="text-red-500">{errors.dob}</small>}
+            {errors.dob && <p className="text-red-500">{errors.dob}</p>}
           </div>
           <div>
-            <label className="block mb-2 text-sm text-gray-300">Address</label>
+            <label htmlFor="address">Address</label>
             <input
               type="text"
+              id="address"
               name="address"
-              placeholder="Enter your address"
-              className="w-full px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white"
               value={formData.address}
               onChange={handleInputChange}
+              className="w-full outline-none px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white focus:border-blue-500"
             />
-            {errors.address && (
-              <small className="text-red-500">{errors.address}</small>
-            )}
+            {errors.address && <p className="text-red-500">{errors.address}</p>}
           </div>
           <div>
-            <label className="block mb-2 text-sm text-gray-300">
-              Phone Number
-            </label>
+            <label htmlFor="phoneNumber">Phone Number</label>
             <input
               type="text"
+              id="phoneNumber"
               name="phoneNumber"
-              placeholder="Enter your phone number"
-              className="w-full px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white"
               value={formData.phoneNumber}
               onChange={handleInputChange}
+              className="w-full outline-none px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white focus:border-blue-500"
             />
             {errors.phoneNumber && (
-              <small className="text-red-500">{errors.phoneNumber}</small>
+              <p className="text-red-500">{errors.phoneNumber}</p>
             )}
           </div>
           <div>
-            <label className="block mb-2 text-sm text-gray-300">
-              Email address
-            </label>
+            <label htmlFor="userEmail">Email</label>
             <input
               type="email"
+              id="userEmail"
               name="userEmail"
-              placeholder="Enter your email address"
-              className="w-full px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white"
               value={formData.userEmail}
               onChange={handleInputChange}
+              className="w-full outline-none px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white focus:border-blue-500"
             />
             {errors.userEmail && (
-              <small className="text-red-500">{errors.userEmail}</small>
+              <p className="text-red-500">{errors.userEmail}</p>
             )}
           </div>
           <div>
-            <label className="block mb-2 text-sm text-gray-300">Password</label>
+            <label htmlFor="userPassword">Password</label>
             <input
               type="password"
+              id="userPassword"
               name="userPassword"
-              placeholder="Enter your password"
-              className="w-full px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white"
               value={formData.userPassword}
               onChange={handleInputChange}
+              className="w-full outline-none px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white focus:border-blue-500"
             />
             {errors.userPassword && (
-              <small className="text-red-500">{errors.userPassword}</small>
+              <p className="text-red-500">{errors.userPassword}</p>
             )}
           </div>
           <div>
-            <label className="block mb-2 text-sm text-gray-300">
-              Confirm Password
-            </label>
+            <label htmlFor="confirmPassword">Confirm Password</label>
             <input
               type="password"
+              id="confirmPassword"
               name="confirmPassword"
-              placeholder="Confirm your password"
-              className="w-full px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white"
               value={formData.confirmPassword}
               onChange={handleInputChange}
+              className="w-full outline-none px-3 py-2 border rounded-md border-gray-500 bg-gray-700 text-white focus:border-blue-500"
             />
             {errors.confirmPassword && (
-              <small className="text-red-500">{errors.confirmPassword}</small>
+              <p className="text-red-500">{errors.confirmPassword}</p>
             )}
           </div>
-          <div className="md:col-span-2 pt-1">
-            <button
-              type="submit"
-              className="w-full px-8 btn transition-colors duration-300 transform py-2 bg-green-600 hover:bg-green-700 text-white rounded-md text-xl shadow-md hover:shadow-lg">
-              Register
-            </button>
-          </div>
+          <button type="submit" className="bg-green-600 p-2 rounded">
+            Register
+          </button>
         </form>
+      </div>
+
+      <div className="mt-10 w-full max-w-2xl">
+        <h2 className="text-2xl font-bold mb-4">Registered Users</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user._id} className="bg-gray-700 p-2 mb-2 rounded">
+              {user.firstName} {user.lastName} - {user.userEmail}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
